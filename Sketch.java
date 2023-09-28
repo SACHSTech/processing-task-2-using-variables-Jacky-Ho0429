cf9b804e8cbc3137102f6a01b5b0fdfc3f0564bd
Jacky-Ho0429 <jacky.ho25@ycdsbk12.ca>
2023-09-28 15:53:51 -0400

    Scaleable House

--- Sketch.java
+++ Sketch.java
@@ -13,43 +13,42 @@ public class Sketch extends PApplet {
 
   //Sets Up Inital Setup
   public void setup() {
-    background(210, 255, 173);
+    background(135, 206, 235);
   }
 
   /**
    * Draws A House on A Patch Of Grass (No Car)
    */
   public void draw() {
-   background(220);
+
+    //House
+    stroke(0);
+    fill(240, 250, 190);
+    rect((float)(width / 16), (float)(height / 1.38), (float)(width / 3.34), (float)(height/4));
   
-   // House
-   stroke(0);
-   fill(240, 250, 190);
-   rect((float)(width / 16), (float)(height / 1.38), (float)(width / 3.34), (float)(height/4));
-  
-   // Roof
-   fill(210, 100, 75);
-   triangle(0, (float)(height / 1.38), (float)(width / 2.35), (float)(height / 1.38), (float)(width / 4.71), (float)(height / 1.86));
+    // Roof
+    fill(210, 100, 75);
+    triangle(0, (float)(height / 1.38), (float)(width / 2.35), (float)(height / 1.38), (float)(width / 4.71), (float)(height / 1.86));
     
-   // Window
-   stroke(255);
-   fill(140, 220, 240);
-   rect((float)(width / 8), (float)(height / 1.23), (float)(width / 13.33), (float)(width / 13.33));
-   line((float)(width / 6.15), (float)(height / 1.23), (float)(width / 6.15), (float)(height / 1.13));
-   line((float)(width / 8), (float)(height / 1.18), (float)(width / 5), (float)(height / 1.18));
+    // Window
+    stroke(255);
+    fill(140, 220, 240);
+    rect((float)(width / 8), (float)(height / 1.23), (float)(width / 13.33), (float)(width / 13.33));
+    line((float)(width / 6.15), (float)(height / 1.23), (float)(width / 6.15), (float)(height / 1.13));
+    line((float)(width / 8), (float)(height / 1.18), (float)(width / 5), (float)(height / 1.18));
     
-   // Door
-   stroke(0);
-   fill(170, 105, 50);
-   rect((float)(width / 4.12), (float)(height / 1.18), (float)(width / 16), (float)(height / 8));
-   stroke(255, 140, 105);
-   fill(255, 140, 105);
-   ellipse((float)(width / 3.48), (float)(height / 1.10), (float)(width / 133.33), (float)(width / 133.33));
+    // Door
+    stroke(0);
+    fill(170, 105, 50);
+    rect((float)(width / 4.12), (float)(height / 1.18), (float)(width / 16), (float)(height / 8));
+    stroke(255, 140, 105);
+    fill(255, 140, 105);
+    ellipse((float)(width / 3.48), (float)(height / 1.10), (float)(width / 133.33), (float)(width / 133.33));
    
-   // Grass
-   stroke(50, 170, 65);
-   fill(50, 170, 65);
-   rect(0, (float)(height/1.03), width, (float)(height/1.03));
+    // Grass
+    stroke(50, 170, 65);
+    fill(50, 170, 65);
+    rect(0, (float)(height/1.03), width, (float)(height/1.03));
   }
   // define other methods down here.
 }
